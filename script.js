5d2ddd74785b626ae26f4763e15737b0a61b7760
André Reis <101909552+andrereissz@users.noreply.github.com>
2026-01-21 11:59:51 -0300

    refactor: reorganiza as imagens da galeria e corrige os iframes

--- script.js
+++ script.js
@@ -51,11 +51,12 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: "Unidade Santa Cruz",
             neighborhood: "Santa Cruz - Rio de Janeiro",
-            address: "Est. Vasconcelos, 1 - Loja B, CEP 23.530-814",
+            address: "Est. Vasconcelos, 480 - Loja B, CEP 23.530-814",
             phone: "(21) 3309-3966",
             cnpj: "12.057.989/0002-30",
             email: "legalizacao.scgroup@gmail.com",
-            imgId: 1
+            imgId: 1,
+            iframe: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d892.2990686869279!2d-43.69069281644765!3d-22.958266011157118!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bf09486f42417%3A0xbf2f4e98fc928c65!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Nova%20Sepetiba!5e0!3m2!1sen!2sus!4v1769006208748!5m2!1sen!2sus"
         },
         {
             name: "Unidade Sepetiba I",
@@ -64,7 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2583-4263",
             cnpj: "12.235.692/0001-36",
             email: "contato@redehiperdrogas.com.br",
-            imgId: 2
+            imgId: 2,
+            iframe: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d772.2634141902585!2d-43.706235176461895!3d-22.967729435035416!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bf0d0f27f1439%3A0x7bd1161ad9131290!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Sepetiba!5e0!3m2!1spt-BR!2sbr!4v1769006287634!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Guaratiba (Magarça)",
@@ -73,7 +75,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 3271-6675",
             cnpj: "13.260.414/0001-00",
             email: "contato@redehiperdrogas.com.br",
-            imgId: 3
+            imgId: 3,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d624.4973874466574!2d-43.598775750290706!3d-22.946646214655424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be5a784de3625%3A0xcc02267b666d53a0!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Magar%C3%A7a!5e0!3m2!1spt-BR!2sbr!4v1769006338353!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Campo Grande",
@@ -82,7 +85,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2222-2222",
             cnpj: "12.057.989/0008-25",
             email: "departamentolegalfarma@outlook.com",
-            imgId: 4
+            imgId: 4,
+            iframe: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1092.2427036963065!2d-43.57596663935559!3d-22.955680899674203!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be5306448e6a3%3A0x9ba479f385f3afef!2sDrogaria%20Ultra%20Popular%20-%20Cachamorra%20I%20Farm%C3%A1cia%20Delivery!5e0!3m2!1spt-BR!2sbr!4v1769006376615!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Guaratiba (Ilha)",
@@ -91,16 +95,18 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2222-2222",
             cnpj: "12.057.989/0007-44",
             email: "departamentolegalfarma@outlook.com",
-            imgId: 5
+            imgId: 5,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1836.4798387318738!2d-43.55417755228649!3d-22.988510199541505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be9c044c9418b%3A0xc8010ad2674b3b3f!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Ilha%20de%20Guaratiba!5e0!3m2!1spt-BR!2sbr!4v1769005644972!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Guaratiba (Matriz)",
             neighborhood: "Guaratiba - Rio de Janeiro",
-            address: "Est. da Matriz, SN - Lt 25 Qd 258, CEP 23.020-715",
+            address: "Est. da Matriz, 2275 - Lt 25 Qd 258, CEP 23.020-715",
             phone: "(21) 3309-3966",
             cnpj: "12.661.254/0001-30",
             email: "legalizacao.scgroup@gmail.com",
-            imgId: 6
+            imgId: 6,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1544.2355374327065!2d-43.6093943173489!3d-22.99321268897369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9befd48cb76fa1%3A0x891f4655ad6e376e!2sDrogarias%20Ultra%20Popular%20-%20Piraqu%C3%AA%20-%20Farm%C3%A1cia%20Delivery!5e0!3m2!1spt-BR!2sbr!4v1769005688865!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Sepetiba II (Piaí)",
@@ -109,7 +115,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2408-8020",
             cnpj: "09.026.944/0001-02",
             email: "cristiano@redehiperdrogas.com.br",
-            imgId: 7
+            imgId: 7,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d651.2287135357899!2d-43.68997859995946!3d-22.97013456068905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bf0ef58488f33%3A0x9735f9114a1fe12b!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Sepetiba!5e0!3m2!1spt-BR!2sbr!4v1769006425931!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Guaratiba (Pedra 1)",
@@ -118,7 +125,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2408-8020",
             cnpj: "12.057.989/0001-59",
             email: "cristiano@redehiperdrogas.com.br",
-            imgId: 8
+            imgId: 8,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d693.1859165970367!2d-43.64825604860811!3d-22.976792902077168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bf1711accec4d%3A0xa413c22607c5064!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20reta%20da%20Pedra%20de%20Guaratiba!5e0!3m2!1spt-BR!2sbr!4v1769006564696!5m2!1spt-BR!2sbr>"
         },
         {
             name: "Unidade Sepetiba III",
@@ -127,16 +135,18 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 9999-9999",
             cnpj: "12.057.989/0006-63",
             email: "departamentolegalfarma@outlook.com",
-            imgId: 9
+            imgId: 9,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d649.7674875083036!2d-43.69392553812225!3d-22.974163198402323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bf126c48708f7%3A0x625256ba75abfd45!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Sepetiba!5e0!3m2!1spt-BR!2sbr!4v1769006030338!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Pedra de Guaratiba",
             neighborhood: "Pedra de Guaratiba - Rio de Janeiro",
-            address: "Est. da Matriz, 000 - Lt 1 Qd 135 Loja A, CEP 23.545-323",
+            address: "Est. da Matriz, 2034 - Lt 1 Qd 135 Loja A, CEP 23.545-323",
             phone: "(21) 2635-4903",
             cnpj: "12.057.989/0005-82",
             email: "legal@farmacon.com.br",
-            imgId: 10
+            imgId: 10,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1091.9241246942108!2d-43.61670524968636!3d-22.995104004961078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9befde0111d8b9%3A0x145645b1c7158c80!2sDrogarias%20Ultra%20Popular%20-%20Rua%20Larga%20Piraqu%C3%AA%20-%20Farm%C3%A1cia%20Delivery!5e0!3m2!1spt-BR!2sbr!4v1769006746955!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Guaratiba (Pilar)",
@@ -145,7 +155,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2408-8020",
             cnpj: "12.057.989/0004-00",
             email: "cristiano@redehiperdrogas.com.br",
-            imgId: 11
+            imgId: 11,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d450.45282205471227!2d-43.60703243520844!3d-22.95548694527811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be56de0794b1d%3A0x99ea609e9dddd9c0!2sDrogarias%20Ultra%20Popular%20-%20Jardim%20Maravilha%20-%20Farm%C3%A1cia%20Delivery!5e0!3m2!1spt-BR!2sbr!4v1769006873381!5m2!1spt-BR!2sbr"
         },
         {
             name: "Unidade Guaratiba (Pedra 2)",
@@ -154,7 +165,8 @@ document.addEventListener('DOMContentLoaded', () => {
             phone: "(21) 2408-8020",
             cnpj: "12.057.989/0003-10",
             email: "cristiano@redehiperdrogas.com.br",
-            imgId: 12
+            imgId: 12,
+            iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1544.3311202554564!2d-43.651182912807926!3d-22.98485368109482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bf030ecf25871%3A0x4d9341034d99b18a!2sDrogarias%20Ultra%20Popular%20-%20Farm%C3%A1cia%20Cabu%C3%ADs!5e0!3m2!1spt-BR!2sbr!4v1769006938932!5m2!1spt-BR!2sbr"
         }
     ];
 
@@ -185,8 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('unit-cnpj').textContent = unit.cnpj;
             document.getElementById('unit-email').textContent = unit.email;
             
-            const mapQuery = encodeURIComponent(unit.address + ", Rio de Janeiro");
-            mapFrame.src = `https://maps.google.com/maps?q=${mapQuery}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
+            mapFrame.src = unit.iframe;
             
             const newImgSrc = `img/unidade_${unit.imgId}.jpg`;
             unitImage.onload = () => {
